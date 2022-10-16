bece744925400433366b3b53be65f6a0cc88867b
Luciano <lucianor.prado@hotmail.com>
2022-10-16 08:26:27 -0300

    feat: Adicionar model Usuario

---
+++ src/GuiaDeConteudo/GuiaDeConteudo/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuiaDeConteudo.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public string cpf_usuario { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar o CPF")]
+        public int tipo { get; set; }
+        public string nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string email { get; set; }
+        public int grauEscolaridade { get; set; }
+        public string formacao { get; set; }
+        public string senha { get; set; }
+
+        //public ICollection<Avaliacao> Avaliacao { get; set; }
+        // public ICollection<Material> Material { get; set; }
+    }
+}
