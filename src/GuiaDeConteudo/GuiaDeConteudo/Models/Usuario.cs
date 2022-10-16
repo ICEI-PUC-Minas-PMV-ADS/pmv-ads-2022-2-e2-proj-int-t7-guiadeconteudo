97e6e505581dd07600a3c32fc796f25ae98b5930
Luciano <lucianor.prado@hotmail.com>
2022-10-16 06:46:38 -0300

    Criação de Primeiro POC - Usuários.

---
+++ src/GuiaDeConteudo/GuiaDeConteudo/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace GuiaDeConteudo.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int ID { get; set; }
+        public string cpf_usuario { get; set; }
+        [Required(ErrorMessage = "Obrigatório Informar o CPF")]
+        public int tipo { get; set; }
+        public string nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string email { get; set; }
+        public int grauEscolaridade { get; set; }
+        public string formacao { get; set; }
+        public string senha { get; set; }
+
+        //public ICollection<Avaliacao> Avaliacao { get; set; }
+        // public ICollection<Material> Material { get; set; }
+    }
+}
