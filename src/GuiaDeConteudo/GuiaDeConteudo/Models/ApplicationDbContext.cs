5e0e576c90298fd98155d645e3bec3a749372fbf
Luciano <lucianor.prado@hotmail.com>
2022-10-16 08:37:40 -0300

    Configuração do EntityFramework, criação do Migration M001 e Update-Database

---
+++ src/GuiaDeConteudo/GuiaDeConteudo/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GuiaDeConteudo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
--- src/GuiaDeConteudo/GuiaDeConteudo/Startup.cs
+++ src/GuiaDeConteudo/GuiaDeConteudo/Startup.cs
@@ -1,6 +1,8 @@
+using GuiaDeConteudo.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,11 @@ namespace GuiaDeConteudo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //adicionado manualmente
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
             services.AddControllersWithViews();
         }
 
