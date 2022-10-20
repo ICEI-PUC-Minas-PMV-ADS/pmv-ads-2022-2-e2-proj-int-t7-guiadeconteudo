483deb12cc07c5201ba5db678c5c0eda0fbe534d
Luciano <lucianor.prado@hotmail.com>
2022-10-20 13:30:16 -0300

    Configuração do Migration M002

--- src/GuiaDeConteudo/GuiaDeConteudo/Migrations/20221020162757_M002.Designer.cs
+++ src/GuiaDeConteudo/GuiaDeConteudo/Migrations/20221020162757_M002.Designer.cs
@@ -4,14 +4,16 @@ using GuiaDeConteudo.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GuiaDeConteudo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221020162757_M002")]
+    partial class M002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GuiaDeConteudo/GuiaDeConteudo/Migrations/20221020162757_M002.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuiaDeConteudo.Migrations
+{
+    public partial class M002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Materiais",
+                columns: table => new
+                {
+                    id_material = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    cpf_usuario = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    areaConhecimento = table.Column<int>(type: "int", nullable: false),
+                    titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    resumo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    autor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    justificativaAnalise = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Usuariocpf_usuario = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materiais", x => x.id_material);
+                    table.ForeignKey(
+                        name: "FK_Materiais_Usuarios_Usuariocpf_usuario",
+                        column: x => x.cpf_usuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "cpf_usuario",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Avaliacoes",
+                columns: table => new
+                {
+                    id_avalicao = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    id_material = table.Column<int>(type: "int", nullable: false),
+                    cpf_usuario = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    rate = table.Column<int>(type: "int", nullable: false),
+                    justificativaAvaliacao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Usuariocpf_usuario = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Materialid_material = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avaliacoes", x => x.id_avalicao);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_Materiais_Materialid_material",
+                        column: x => x.id_material,
+                        principalTable: "Materiais",
+                        principalColumn: "id_material",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_Usuarios_Usuariocpf_usuario",
+                        column: x => x.cpf_usuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "cpf_usuario",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_Materialid_material",
+                table: "Avaliacoes",
+                column: "Materialid_material");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_Usuariocpf_usuario",
+                table: "Avaliacoes",
+                column: "Usuariocpf_usuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materiais_Usuariocpf_usuario",
+                table: "Materiais",
+                column: "Usuariocpf_usuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Avaliacoes");
+
+            migrationBuilder.DropTable(
+                name: "Materiais");
+        }
+    }
+}
