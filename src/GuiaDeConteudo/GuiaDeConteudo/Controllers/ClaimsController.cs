8271f598666bafc64f709aa171416396201a0b33
Luciano <lucianor.prado@hotmail.com>
2022-11-25 21:26:14 -0300

    Passagem de CPF com ID.

---
+++ src/GuiaDeConteudo/GuiaDeConteudo/Controllers/ClaimsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GuiaDeConteudo.Controllers
+{
+    [Authorize]
+    public class ClaimsController : Controller
+    {
+        public ViewResult Index() => View(User?.Claims);
+    }
+}
